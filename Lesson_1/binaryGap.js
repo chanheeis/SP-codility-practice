1f6b33b2586dfe505e36e7ab3d7b495717a44ac6
Chanhee <chanheeis@naver.com>
2019-09-28 17:49:43 +0900

    Lesson_1 binaryGap Successgit add --all

--- Lesson_1/binaryGap.js
+++ Lesson_1/binaryGap.js
@@ -1,34 +1,33 @@
 function binaryGap(testValue){
-    console.log(`=================`);
     if(!isNaN(testValue)&&testValue>8){
-        const binary=testValue.toString(2);
-        console.log(binary);
 
+        const binary=testValue.toString(2);
         let temp=0;
         let max=0;
 
         for(let i=1;i<binary.length;i++){
             if(binary[i]==="0"){
                 temp++;
-            }else{
-                if(binary[i-1]==="0"){
-                    max=temp;
+                if(i<binary.length-1 && binary[i+1]==="1"){
+                    if(max<temp){
+                        max=temp;
+                    }
                     temp=0;
                 }
             }   
         }
-        console.log(`Temp : ${temp}`);
-        console.log(`Max : ${max}`);
+        return max;
     }else{
         return 0;
     }
 }
-console.log('Test Start');
 
-binaryGap(100);
-binaryGap(1000);
-binaryGap(10000);
-binaryGap(1231);
-binaryGap(567);
+console.log(binaryGap(567));
+console.log(binaryGap(1021));
+console.log(binaryGap(1024));
+console.log(binaryGap(98));
+console.log(binaryGap(432));
+console.log(binaryGap(10));
+console.log(binaryGap(1));
 
 
